bfa217408d226ed7a8c8f03a7d58afb9563e5628
Somel Ahmed <progsomel@Somels-MacBook-Pro.local>
2026-08-01 10:40:09 +0600

    more examples of string added

--- 0 - Python/05 - List, Tuple, Dictionary, set and String/main.py
+++ 0 - Python/05 - List, Tuple, Dictionary, set and String/main.py
@@ -1,2 +1,13 @@
-list = [n**2 for n in range(1, 101)]
-print(list)
+name = "Somel"
+age = 28
+
+print("My name is {} and I am {} years old".format(name, age))
+print("My name is %s and I am %d years old" % (name, age))
+
+message = """Hello,
+Welcome to python Strings.
+Let's Learn!
+"""
+
+path = r"C:\Users\Somel\Documents"
+print(path)
